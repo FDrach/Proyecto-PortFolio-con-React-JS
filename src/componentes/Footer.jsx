e01f4076489ad94fa21449696530909d9efd0c5d
Franco Drachenberg <Francodrach@gmail.com>
2025-04-18 21:04:51 -0300

    Footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  let startYear=2025;
+  let curYear=new Date().getFullYear();
   return (
     <footer className="app-footer">
-      <p>Titulo</p>
+      <p> © Copyright {startYear}{curYear === startYear ? "" : "-"+new Date().getFullYear()} Franco Drachenberg</p>
     </footer>
   );
 };
