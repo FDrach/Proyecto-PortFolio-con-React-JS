247ba7d72c064ca11f29f5de0e47ea370dab10bb
Franco Drachenberg <Francodrach@gmail.com>
2025-04-18 21:18:58 -0300

    Componente Header, Nombre y Avatar de GitHub

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <header className="app-header">
-      <h1>Titulo</h1>
+      <img src="https://avatars.githubusercontent.com/FDrach" alt="" class="avatar" />
+      <h1>Franco Eduardo Drachenberg</h1>
+
     </header>
   );
 };
